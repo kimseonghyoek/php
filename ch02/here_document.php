b1a0ae01c6af7dbfc79d6f211b98b3078fc583ce
kimseonghyoek <cilo1223@naver.com>
2020-03-25 02:46:52 +0900

    Create here_document.php

---
+++ ch02/here_document.php
@@ -0,0 +1,13 @@
+<<<HTMLBLOCK
+<html>
+<head><title>메뉴</title></head>
+<body bgcolor="#fffed9">
+    <h1>저녁</h1>
+    <ul>
+    <li> 소고기 쌀국수 볶음</li>
+    <li> 완두싹 볶음</li>
+    <li> 간장 볶음면</li>
+    </ul>
+</body>
+</html>
+HTMLBLOCK
