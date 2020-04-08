72708ccefd47fc47f3e96e40d1d42c5c5d14d74f
kimseonghyoek <cilo1223@naver.com>
2020-04-09 00:07:32 +0900

    Create not_operator.php

---
+++ ch03/not_operator.php
@@ -0,0 +1,13 @@
+<?php
+// ($finished == false)라는 표현식 전체는
+// $finished가 거짓일 때 참이다.
+if ($finished == false) {
+    print '아직 덜 끝났다.';
+}
+
+// (! $finished)라는 표현식 전체는
+// $finished가 거짓일 때 참이다.
+if (! $finished) {
+    print '아직 덜 끝났어!';
+}
+?>
