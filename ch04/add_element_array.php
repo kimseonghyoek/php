8aa72bf3f51b6efdb809929a5571b9c6d4d342e6
kimseonghyoek <cilo1223@naver.com>
2020-04-13 20:24:33 +0900

    Create add_element_array.php

---
+++ ch04/add_element_array.php
@@ -0,0 +1,16 @@
+<?php
+// 문자열 키를 사용하는 $vegetables 배열
+$vegetables['corn'] = 'yellow';
+$vegetables['beet'] = 'red';
+$vegetables['carrot'] = 'orange';
+
+// 숫자키를 이용하는 $dinner 배열
+$dinner[0] = '스위트콘과 아스파라거스';
+$dinner[1] = '레몬 치킨';
+$dinner[2] = '삶은 망태버섯';
+
+// 숫자 키와 문자열 키를 사용하는 $computer 배열
+$computers['nintendo-64'] = '닌텐도';
+$computers[2600] = '아타리';
+$computers['Saturn'] = '세가';
+?>
