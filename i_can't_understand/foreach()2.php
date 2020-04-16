7f6d77dd456b518bfd255c4cbefbfc64918d1e83
kimseonghyoek <cilo1223@naver.com>
2020-04-16 18:10:21 +0900

    Create foreach()2.php

---
+++ i_can't_understand/foreach()2.php
@@ -0,0 +1,11 @@
+<?php
+$employee_list = array(
+    'Programmer' => 'Edward',
+    'Designer' => 'Alex'
+);
+
+foreach($employee_list as $key => $value)
+{
+    echo $key." : ".$value. "<br/>";
+}
+?>
