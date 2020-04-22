be6e4525a8a24cd8ed206b596faa4c619cb085ea
kimseonghyoek <cilo1223@naver.com>
2020-04-22 23:16:04 +0900

    Create 4_14_2.php

---
+++ ch04/4_14_2.php
@@ -0,0 +1,10 @@
+<?php
+$letters[0] = 'A';
+$letters[1] = 'B';
+$letters[3] = 'D';
+$letters[2] = 'C';
+
+for ($i = 0, $num_letters = count($letters); $i < $num_letters; $i++) {
+    print $letters[$i];
+}
+?>
