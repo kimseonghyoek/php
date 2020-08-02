299532498361ab493d32d5b6a48f47862a8e5ba1
Kyunghee <cilo1223@naver.com>
2020-08-02 23:56:27 +0900

    update

---
+++ ch05/5_1.php
@@ -0,0 +1,13 @@
+<?php
+    $user = 'kim';
+
+    function page_header() {
+        $user = "Kim";
+        print '<html><head><title>저의 홈페이지에 오신 것을 환영합니다.</title></head>';
+        print '<body bgcolor="#a03251">';
+    }
+    // 5-2 function call
+    page_header();
+    print "어서오세요, $user 님. ";
+    print "</body></html>";
+?>
